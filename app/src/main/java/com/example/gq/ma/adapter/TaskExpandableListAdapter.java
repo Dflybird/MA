ed8c8227ef5fcef8bbde976e64c2e7279a0b8f22
gq <dflybirds@gmail.com>
2018-04-23 00:52:29 +0800

    周末

--- app/src/main/java/com/example/gq/ma/adapter/TaskExpandableListAdapter.java
+++ app/src/main/java/com/example/gq/ma/adapter/TaskExpandableListAdapter.java
@@ -7,173 +7,114 @@ import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import com.example.gq.ma.R;
+import com.example.gq.ma.bean.Task;
+
 import java.util.List;
 
 public class TaskExpandableListAdapter extends BaseExpandableListAdapter {
 
+    private Context context;
     private String groupStrings[] = {"探测", "运输"};
+    private List<Task> detectTasks;
+    private List<Task> transportTasks;
+
+    public TaskExpandableListAdapter(Context context, List<Task> detectTasks, List<Task> transportTasks) {
+        this.context = context;
+        this.detectTasks = detectTasks;
+        this.transportTasks = transportTasks;
+    }
 
     @Override
     public int getGroupCount() {
-        return 0;
+        return 2;
     }
 
     @Override
     public int getChildrenCount(int groupPosition) {
-        return 0;
+        if (groupPosition == 0)
+            return detectTasks.size();
+        else if (groupPosition == 1)
+            return transportTasks.size();
+        else return 0;
     }
 
     @Override
     public Object getGroup(int groupPosition) {
-        return null;
+        return groupStrings[groupPosition];
     }
 
     @Override
     public Object getChild(int groupPosition, int childPosition) {
-        return null;
+        if (groupPosition == 0)
+            return detectTasks.get(childPosition);
+        else if (groupPosition == 1)
+            return transportTasks.get(childPosition);
+        else return null;
     }
 
     @Override
     public long getGroupId(int groupPosition) {
-        return 0;
+        return groupPosition;
     }
 
     @Override
     public long getChildId(int groupPosition, int childPosition) {
-        return 0;
+        if (groupPosition == 0)
+            return detectTasks.get(childPosition).getId();
+        else if (groupPosition == 1)
+            return transportTasks.get(childPosition).getId();
+        else return 0;
     }
 
     @Override
     public boolean hasStableIds() {
-        return false;
+        return true;
     }
 
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        return null;
+        GroupViewHolder groupViewHolder;
+        if (convertView == null) {
+            groupViewHolder = new GroupViewHolder();
+            convertView = LayoutInflater.from(context).inflate(R.layout.task_expandable_item, parent, false);
+            groupViewHolder.groupTV = convertView.findViewById(R.id.task_expandable_tv);
+            convertView.setTag(groupViewHolder);
+        }
+        else
+            groupViewHolder = (GroupViewHolder) convertView.getTag();
+        groupViewHolder.groupTV.setText(groupStrings[groupPosition]);
+        return convertView;
     }
 
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        return null;
+        ChildViewHolder childViewHolder;
+        if (convertView == null){
+            convertView = LayoutInflater.from(context).inflate(R.layout.task_expandable_item,parent,false);
+            childViewHolder = new ChildViewHolder();
+            childViewHolder.childTV = convertView.findViewById(R.id.task_expandable_tv);
+            convertView.setTag(childViewHolder);
+        }
+        else
+            childViewHolder = (ChildViewHolder) convertView.getTag();
+        if (groupPosition == 0)
+            childViewHolder.childTV.setText(detectTasks.get(childPosition).getTitle());
+        else if (groupPosition == 1)
+            childViewHolder.childTV.setText(transportTasks.get(childPosition).getTitle());
+        return convertView;
     }
 
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
-        return false;
+        return true;
     }
 
-//
-//    private String groupStrings[] = {"发现", "攻击"};
-//    private List<Task> attackTask = new ArrayList<>();
-//    private List<Task> investigateTask = new ArrayList<>();
-//
-//    private int investigateGroupCount;
-//    private int attackGroupCount;
-//
-//    private Context context;
-//
-//    public MyExpandableListAdapter(List<Task> list, Context context) {
-//        this.context = context;
-//        for (Task t:list) {
-//            if (t.getType() == Task.TaskType.INVESTIGATE)
-//                investigateTask.add(t);
-//            else attackTask.add(t);
-//        }
-//        investigateGroupCount = investigateTask.size();
-//        attackGroupCount = attackTask.size();
-//    }
-//
-//    @Override
-//    public int getGroupCount() {
-//        return groupStrings.length;
-//    }
-//
-//    @Override
-//    public int getChildrenCount(int groupPosition) {
-//        return groupPosition == INVESTIGATE_GROUP ?
-//                investigateGroupCount : attackGroupCount;
-//    }
-//
-//    @Override
-//    public Object getGroup(int groupPosition) {
-//        return groupStrings[groupPosition];
-//    }
-//
-//    @Override
-//    public Object getChild(int groupPosition, int childPosition) {
-//        return groupPosition == INVESTIGATE_GROUP ?
-//                investigateTask.get(childPosition) :
-//                attackTask.get(childPosition);
-//    }
-//
-//    @Override
-//    public long getGroupId(int groupPosition) {
-//        return groupPosition;
-//    }
-//
-//    @Override
-//    public long getChildId(int groupPosition, int childPosition) {
-//        return childPosition;
-//    }
-//
-//    @Override
-//    public boolean hasStableIds() {
-//        return true;
-//    }
-//
-//    @Override
-//    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-//        GroupViewHolder groupViewHolder;
-//        if (convertView == null) {
-//            convertView = LayoutInflater.from(context).inflate(R.layout.expandable_item, parent, false);
-//            groupViewHolder = new GroupViewHolder();
-//            groupViewHolder.tvTitle = convertView.findViewById(R.id.expand_item_tv);
-//            convertView.setTag(groupViewHolder);
-//        } else
-//            groupViewHolder = (GroupViewHolder) convertView.getTag();
-//        String tvStr;
-//        if (groupPosition == INVESTIGATE_GROUP)
-//            tvStr = "侦查任务";
-//        else tvStr = "攻击任务";
-//        groupViewHolder.tvTitle.setText(tvStr);
-//        return convertView;
-//    }
-//
-//    @Override
-//    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-//        ChildViewHolder childViewHolder;
-//        if (convertView == null) {
-//            convertView = LayoutInflater.from(context).inflate(R.layout.expandable_item, parent,false);
-//            childViewHolder = new ChildViewHolder();
-//            childViewHolder.tvTitle = convertView.findViewById(R.id.expand_item_tv);
-//            convertView.setTag(childViewHolder);
-//        } else
-//            childViewHolder = (ChildViewHolder) convertView.getTag();
-//
-//        StringBuilder builder = new StringBuilder();
-//        builder.append(groupStrings[groupPosition]);
-//        if (groupPosition == INVESTIGATE_GROUP) {
-//            builder.append(investigateTask.get(childPosition).getInvestigateTarget().getName());
-//        } else {
-//            builder.append(attackTask.get(childPosition).getAttackTarget().getName());
-//        }
-//        String tvStr = builder.toString();
-//        childViewHolder.tvTitle.setText(tvStr);
-//        return convertView;
-//    }
-//
-//    @Override
-//    public boolean isChildSelectable(int groupPosition, int childPosition) {
-//        return true;
-//    }
-//
-//    class GroupViewHolder {
-//        TextView tvTitle;
-//    }
-//
-//    class ChildViewHolder {
-//        TextView tvTitle;
-//    }
+    class GroupViewHolder{
+        TextView groupTV;
+    }
+
+    class ChildViewHolder{
+        TextView childTV;
+    }
 }
--- app/src/main/java/com/example/gq/ma/bean/Task.java
+++ app/src/main/java/com/example/gq/ma/bean/Task.java
@@ -4,6 +4,15 @@ public class Task {
 
     private int id;
     private String date;
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     public int getId() {
         return id;
